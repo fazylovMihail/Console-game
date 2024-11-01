ae6c5f53b3a940346695b7b1588ee591122a7e90
Fazylov Mihail <161601509+fazylovMihail@users.noreply.github.com>
2024-11-01 15:19:00 +0500

    Написание базы нашей игры

---
+++ script.js
@@ -0,0 +1,23 @@
+console.log('Я загадал число от 1 до 10. Попробуй угадать его!');
+
+let secretNum = Math.floor(Math.random()*10+1);
+let attamtes = 3;
+
+while(attamtes>0){
+    let userNum = Number(prompt('Введите число: '));
+
+    if(userNum < secretNum){
+        attamtes--;
+        console.log('Секретное число больше.');
+        console.log('У вас осталось', attamtes)
+    }
+    else if(userNum == secretNum){
+        console.log('Вы угадали!');
+        break;
+    }
+    else{
+        attamtes--;
+        console.log('Секретное число меньше.');
+        console.log('У вас осталось', attamtes, 'попытки!')
+    }
+}
